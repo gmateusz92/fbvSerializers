d6017a0246f22de26457db410d750f731efa12b8
Mateusz <gmateusz92@gmail.com>
2024-03-20 22:09:49 +0100

    1

--- fbvApp/serializers.py
+++ fbvApp/serializers.py
@@ -4,4 +4,4 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['id', 'name', 'sal']
+        fields = ['id', 'name', 'score']
--- fbvApp/views.py
+++ fbvApp/views.py
@@ -9,35 +9,35 @@ from rest_framework.decorators import api_view
 @api_view(['GET','POST'])
 def student_list(request):
 
-    if request.method == 'GET':
-        students= Student.objects.all()
-        serializer = StudentSerializer(students, many=True)
+    if request.method =='GET':
+        students = Student.objects.all()
+        serializer=StudentSerializer(students,many=True)
         return Response(serializer.data)
-    
+
     elif request.method == 'POST':
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET','PUT','POST'])
-def student_detail(request, pk):
+@api_view(['GET','PUT','DELETE'])
+def student_detail(request,pk):
     try:
-        student = Student.objects.all(pk=pk)
+        student = Student.objects.get(pk=pk)
     except Student.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    
+
     if request.method=='GET':
-        serializer= StudentSerializer(student)
+        serializer = StudentSerializer(student)
         return Response(serializer.data)
-    
-    elif request.method =='PUT':
-        serializer = StudentSerializer(student, data=request.data)
+
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)    
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         student.delete()
